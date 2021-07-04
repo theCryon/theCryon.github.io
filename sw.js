2cbc4016e05b706b23523dfee8ac37f82ead87e3
theCryon <66066260+theCryon@users.noreply.github.com>
2021-07-04 12:35:31 +0200

    b

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 var cacheName = 'piac-pwa';
 var filesToCache = ['/','/index.html','/style.css','/js/main.js', '/js/router.js', 'images/buy.png', 'images/cloud-network.png', 
-'images/house.png', 'images/socket.png'];
+'images/house.png', 'images/socket.png', 'images/apple.png', 'images/maskable-icon-256.png'];
 
 self.addEventListener('install', function(e) {
 	e.waitUntil(caches.open(cacheName).then(function(cache) {
